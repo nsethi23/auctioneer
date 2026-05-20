6ab3b1db3cc469c2c891bacdad8a2be4e37c4953
nsethi23 <165092420+nsethi23@users.noreply.github.com>
2026-05-20 12:36:39 -0500

    chore: add URL to main.py

--- backend/auctioneer/api/main.py
+++ backend/auctioneer/api/main.py
@@ -26,6 +26,7 @@ app.add_middleware(
     allow_origins=[
         "http://127.0.0.1:5173",
         "http://localhost:5173",
+        "https://auctioneer-five.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
